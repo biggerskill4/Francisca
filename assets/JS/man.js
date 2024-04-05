e7673fc333454072a597b447ac91ac040a8e341a
John Doe <johndoe@email.com>
2024-04-05 20:02:27 +0500

    menu updated

--- assets/JS/man.js
+++ assets/JS/man.js
@@ -1,7 +1,7 @@
 const navigation = document.querySelectorAll(".link");
 const dataPage = document.querySelector('[data-page]');
 navigation.forEach((a) => {
-  if (a.textContent == dataPage.getAtttibut('data-page')) {
+  if (a.textContent == dataPage.getAttribute('data-page')) {
     a.classList.add("active");
   }
 });
